df9fc19423ed0f2ba0de4c1269cc030456406aa2
Yuri Pereira <bitbucket4@cerbrum.com>
2022-02-05 10:56:00 -0300

    [Atualização] Singleton

---
+++ dotnet-project/Db/SingletonDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace dotnet_project.Db
+{
+    public class SingletonDb
+    {
+        /// <summary>
+        /// O objeto intance é a instância única da própria classe SingletonConexao;
+        /// Seu modificador "readonly", determina que o mesmo só pode ser alterado na sua declaração ou dentro de um método construtor.
+        /// </summary>
+        private static readonly SingletonDb instance = new SingletonDb();
+
+        /// <summary>
+        /// Construtor privado para não permir que outras classes consiguam estaciar a SingletonConexao diretamente.
+        /// </summary>
+        private SingletonDb() { }
+
+        /// <summary>
+        /// Este método é a única forma das classes externas conseguirem acessar está classe.
+        /// </summary>
+        /// <returns>Retorna o variavel instance, que contém a instancia desta classe.</returns>
+        public static SingletonDb getInstance()
+        {
+            return instance;
+        }
+        /// <summary>
+        /// Este método a responsavel por instanciar uma nova conexão, passando por parâmetro a String de conexão.
+        /// </summary>
+        /// <returns>Retorna uma conexão com o banco de dados já preparada para se conectar.</returns>
+        public static SqlConnection getConnection()
+        {
+            SqlConnection sqlConn = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=db_aspnet;Data Source=DESKTOP-A5LEM6K\SQLEXPRESS");
+            return sqlConn;
+        }
+    }
+}
---
+++ dotnet-project/Facade/AbstractFacade.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using dotnet_project.Db;
+using SqlKata;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace dotnet_project.Facade
+{
+    public abstract class AbstractFacade
+    {
+        protected SqlConnection conn;
+
+        public AbstractFacade()
+        {
+            conn = SingletonDb.getConnection();
+        }
+
+        /// <summary>
+        /// Realiza um Select generico
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        protected List<T> Select<T>(string sql, object param = null)
+        {
+            try
+            {
+                return conn.Query<T>(sql, param).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void OpenConnection()
+        {
+            if (conn?.State != ConnectionState.Open)
+                conn?.Open();
+        }
+
+        protected void CloseConnection()
+        {
+            if (conn?.State != ConnectionState.Closed)
+                conn?.Close();
+        }
+
+        
+    }
+
+    public static class AbstractFacadeMethods
+    {
+        public static List<T> ToList<T>(this Query query)
+        {
+            return query.Get<T>().ToList();
+        }
+
+        public static T FirstOrDefault<T>(this Query query)
+        {
+            return query.Get<T>().FirstOrDefault();
+        }
+
+        public static bool Any(this Query query)
+        {
+            return query.Get().Any();
+        }
+
+        public static bool Any<T>(this Query query)
+        {
+            return query.Get<T>().Any();
+        }
+}
