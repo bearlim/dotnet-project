de546fd08e02a1800fd0ae5ae4d175d03d5ca6f3
Yuri Pereira <bitbucket4@cerbrum.com>
2022-02-05 21:14:00 -0300

    [Atualização] Novos pacotes e atualizado routes

--- dotnet-project/App_Start/RouteConfig.cs
+++ dotnet-project/App_Start/RouteConfig.cs
@@ -14,9 +14,19 @@ namespace dotnet_project
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "usuario",
+                "usuario",
+                new { controller = "Usuario", action = "Index" });
+
+            routes.MapRoute(
+                "cadastrar-se",
+                "cadastrar-se",
+                new { controller = "Account", action = "Signup" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
