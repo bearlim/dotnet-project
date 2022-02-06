6ace0c5a2b0b5c47e7b7787bd5e8a468bc2de022
Yuri Pereira <bitbucket4@cerbrum.com>
2022-02-05 21:16:10 -0300

    [Atualização]

---
+++ dotnet-project/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dotnet_project.Models;
+using System.Web.Security;
+
+namespace dotnet_project.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using (var context = new UsuarioEntities())
+            {
+                bool isValid = context
+                    .Users
+                    .Any(x => x.nmLogin == user.nmLogin && x.dsSenha == user.dsSenha);
+
+                var obj = context.Users.Where(x => x.nmLogin == user.nmLogin && x.dsSenha == user.dsSenha);
+
+                if (isValid && obj != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.nmLogin, false);
+                    Session["nmLogin"] = user.nmLogin;
+                    return RedirectToAction("Index", "Usuarios");
+                }
+
+                ModelState.AddModelError("", "Usuário ou senha incorretos");
+                return View();
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Users user)
+        {
+            using (var context = new UsuarioEntities())
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ dotnet-project/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace dotnet_project.Models
+{
+    public class User
+    {
+        [Key]
+        public int idUser { get; set; }
+        [Display(Name = "Usuário", ShortName = "User")]
+        public string nmLogin { get; set; }
+        [Display(Name = "Senha", ShortName = "Pass")]
+        public string dsSenha { get; set; }
+
+    }
+}
